6ea61286a4c533f97684be55e80fa4b69b8bfbfa
raulugarte <63106469+raulugarte@users.noreply.github.com>
2024-10-07 23:25:42 +0200

    Update feature.js

--- blocks/feature/feature.js
+++ blocks/feature/feature.js
@@ -1,5 +1,25 @@
 import { fetchPlaceholders } from '../../scripts/lib-franklin.js';
 
+export function createElement(tagName, options = {}) {
+  const { classes = [], props = {} } = options;
+  const elem = document.createElement(tagName);
+  const isString = typeof classes === 'string';
+  if (classes || (isString && classes !== '') || (!isString && classes.length > 0)) {
+    const classesArr = isString ? [classes] : classes;
+    elem.classList.add(...classesArr);
+  }
+  if (!isString && classes.length === 0) elem.removeAttribute('class');
+
+  if (props) {
+    Object.keys(props).forEach((propName) => {
+      const value = propName === props[propName] ? '' : props[propName];
+      elem.setAttribute(propName, value);
+    });
+  }
+
+  return elem;
+}
+
 export default async function decorate(block) {
   const mediaWrapper = document.createElement('div');
   mediaWrapper.classList.add('feature-content-media');
